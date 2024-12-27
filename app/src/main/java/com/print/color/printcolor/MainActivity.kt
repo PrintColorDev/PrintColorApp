4127e4d70a0079ccc20d83f08967b12082adc34e
2350348 <DGonzalezQuezada@carnival.com>
2024-12-26 21:53:08 -0600

    App Theme color version 1

--- app/src/main/java/com/print/color/printcolor/MainActivity.kt
+++ app/src/main/java/com/print/color/printcolor/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,10 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrintColorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Column {
+                    Button(onClick = {}) {
+                        Text("ajkhsdgfb")
+                    }
                     Greeting(
                         name = "Android Test",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
                     )
                 }
             }
