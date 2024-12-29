378791758550fb84e3c60420a3b28ebf87426825
2350348 <DGonzalezQuezada@carnival.com>
2024-12-28 20:16:14 -0600

    data

--- app/src/main/java/com/print/color/printcolor/ui/components/TextFieldTheme/model/TextFieldData.kt
+++ app/src/main/java/com/print/color/printcolor/ui/components/TextFieldTheme/model/TextFieldData.kt
@@ -3,6 +3,14 @@ package com.print.color.printcolor.ui.components.TextFieldTheme.model
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 
+/**
+ * @param textFieldType: The type of the text field.
+ * @param label: The label of the text field.
+ * @param placeHolder: The placeholder of the text field.
+ * @param keyboardType: The type of keyboard to be used.
+ * @param leadingIcon: The leading icon of the text field.
+ * */
+
 data class TextFieldData(
     val textFieldType: TextFieldType,
     val label: String,
