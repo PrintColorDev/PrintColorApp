5440fdd20f46fb9b6c9306829ba8f2fe46b2a67d
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2025-03-01 22:26:30 -0600

    searchBar row bugfix and libraries update

--- app/src/main/java/com/print/color/printcolor/ui/quotationList/QuotationListScreen.kt
+++ app/src/main/java/com/print/color/printcolor/ui/quotationList/QuotationListScreen.kt
@@ -2,12 +2,19 @@ package com.print.color.printcolor.ui.quotationList
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -23,10 +30,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.print.color.printcolor.R
 import com.print.color.printcolor.domain.model.Quotation
@@ -59,25 +68,39 @@ fun QuotationListScreen(
                 .fillMaxSize()
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Min),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
             ) {
-                PcsTextField(
-                    data = TextFieldData(
-                        textFieldType = OUTLINED,
-                        label = "",
-                        placeHolder = stringResource(R.string.quotation_list_screen_search_bar),
-                        keyboardType = KeyboardType.Text,
-                        leadingIcon = painterResource(R.drawable.ic_pcs_search)
-                    ),
-                    modifier = Modifier,
-                    onValueChange = { searchBarText = it },
-                    value = searchBarText,
-                    imeAction = ImeAction.Search
-                )
-                IconButton(onClick = {}) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    PcsTextField(
+                        data = TextFieldData(
+                            textFieldType = OUTLINED,
+                            label = "",
+                            placeHolder = stringResource(R.string.quotation_list_screen_search_bar),
+                            keyboardType = KeyboardType.Text,
+                            leadingIcon = painterResource(R.drawable.ic_pcs_search)
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
+                        onValueChange = { searchBarText = it },
+                        value = searchBarText,
+                        imeAction = ImeAction.Search
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .align(Alignment.CenterVertically)
+                ) {
                     Icon(
-                        modifier = modifier,
                         painter = painterResource(R.drawable.ic_pcs_filter),
                         contentDescription = "Filter",
                         tint = MaterialTheme.colorScheme.onTertiary
